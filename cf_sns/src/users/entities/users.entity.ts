4412903047d93e74a5dc3d091079b101d9db558c
heajun jeong <heajun35@gmail.com>
2025-06-12 20:55:02 +0900

    Feat: Exclude Annotation practice

--- cf_sns/src/users/entities/users.entity.ts
+++ cf_sns/src/users/entities/users.entity.ts
@@ -6,6 +6,7 @@ import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -24,6 +25,19 @@ export class UsersModel extends BaseModel {
   @Column()
   @IsString({ message: stringValidationMessage })
   @Length(3, 8, { message: lengthValidationMessage })
+  /**
+   * Request
+   * frontend -> backend
+   * plain object (JSON) -> class instance (dto)
+   *
+   * Response
+   * backend -> frontend
+   * class instance (entity) -> plain object (JSON)
+   *
+   * toClassOnly -> class instance로 변환될떄만 (요청)
+   * toPlainOnly -> plain object로 변환될때만 (응답)
+   */
+  @Exclude({ toPlainOnly: true })
   password: string;
 
   @Column({ enum: Object.values(RolesEnum), default: RolesEnum.USER })
--- cf_sns/src/users/users.controller.ts
+++ cf_sns/src/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -6,6 +7,11 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  /**
+   * serialization -> 직렬화 -> 현재 시스템에서 사용되는 (NestJS) 데이터의 구조를 다른 시스템에서도 쉽게 사용 할 수 있는 포맷으로 변환
+   *               -> class의 object를 JSON 포맷으로 변환
+   * deserialization -> 역직렬화
+   */
   getUsers() {
     return this.usersService.getAllUsers();
   }

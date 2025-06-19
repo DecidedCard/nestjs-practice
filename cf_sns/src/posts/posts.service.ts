0da34e32ac4f40b2db03dee567a07d06b54039c9
heajun jeong <heajun35@gmail.com>
2025-06-20 00:27:02 +0900

    Feat: Override Options 적용

--- cf_sns/src/posts/posts.service.ts
+++ cf_sns/src/posts/posts.service.ts
@@ -34,7 +34,12 @@ export class PostsService {
 
   // 오름차 순으로 정렬하는 pagination만 구현한다.
   async paginatePosts(dto: paginatePostDto) {
-    return this.commonService.paginate(dto, this.postsRepository, {}, 'posts');
+    return this.commonService.paginate(
+      dto,
+      this.postsRepository,
+      { relations: ['author'] },
+      'posts',
+    );
     // if (dto.page) {
     //   return this.pagePaginatePosts(dto);
     // } else {

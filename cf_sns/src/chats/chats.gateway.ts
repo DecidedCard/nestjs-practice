0a15b6c51205e45f12ba99dafaae161a42380cd2
jung hea jun <heajun35@gmail.com>
2025-08-11 22:13:37 +0900

    Feat: Room 활용하기

--- cf_sns/src/chats/chats.gateway.ts
+++ cf_sns/src/chats/chats.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
@@ -18,9 +19,22 @@ export class ChatsGateway implements OnGatewayConnection {
     console.log(`on connect called : ${socket.id}`);
   }
 
+  @SubscribeMessage('enter_chat')
+  enterChat(@MessageBody() data: number[], @ConnectedSocket() socket: Socket) {
+    for (const chatId of data) {
+      // socket.join()
+      socket.join(chatId.toString());
+    }
+  }
+
   // socket.on('send_message', (message) => {console.log(message)})
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() message: string) {
-    this.server.emit('receive_message', 'hello from server');
+  sendMessage(
+    @MessageBody() message: { message: string; chatId: number },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    this.server
+      .in(message.chatId.toString())
+      .emit('receive_message', message.message);
   }
 }

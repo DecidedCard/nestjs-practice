b76f8c4b2598a2d70d8ea92dd4bab757aa1a9e2d
jung hea jun <heajun35@gmail.com>
2025-09-01 22:50:51 +0900

    Feat: 배포 테스트

--- cf_sns/src/app.controller.ts
+++ cf_sns/src/app.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './common/decorator/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @IsPublic()
+  get() {
+    return this.appService.getHello();
+  }
 }
